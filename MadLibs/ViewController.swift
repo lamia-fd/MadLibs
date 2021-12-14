411fe9dceb9bfea8e4de37ee35b8db9da369fa60
lamia <lamia.fd123@gmail.com>
2021-12-14 13:22:56 +0300

    v1

--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+var adjText=""
+    var verb1Text=""
 
+    var verb2Text=""
+
+   var nounText=""
+
+    @IBAction func nextpage(_ sender: UIBarButtonItem) {
+        let cv=storyboard?.instantiateViewController(withIdentifier: "secondCVid") as! secondCV
+        
+        self.navigationController?.pushViewController(cv, animated: true)
+       
+    }
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+
+    @IBAction func back(_ Segue: UIStoryboardSegue){
+        label.text="We are having a perfectly \(adjText) right now. Later we will \(verb1Text) and \(verb2Text) in the \(nounText)"
+        
+        
+      //  print(adjText)
     }
 
 
---
+++ MadLibs/secondCV.swift
@@ -0,0 +1,53 @@
+//
+//  secondCV.swift
+//  MadLibs
+//
+//  Created by لمياء فالح الدوسري on 10/05/1443 AH.
+//
+
+import UIKit
+
+class secondCV: UIViewController {
+
+    @IBOutlet weak var noun: UITextField!
+    @IBOutlet weak var verb2: UITextField!
+    @IBOutlet weak var verb1: UITextField!
+    @IBOutlet var adj: UITextField!
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let vc = segue.destination as? ViewController{
+        
+    vc.adjText="\(adj.text!)"
+    vc.verb1Text="\(verb1.text!)"
+    vc.verb2Text="\(verb2.text!)"
+    vc.nounText="\(noun.text!)"
+    
+    
+}}
+    
+    @IBAction func submit(_ sender: UIButton) {
+        
+        let home=storyboard?.instantiateViewController(withIdentifier:"homeVC")as! ViewController
+      
+        
+      //  home.adjText = adj.text!
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
